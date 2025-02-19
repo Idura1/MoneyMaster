8a3330e882a0ee3abb0681385faf1e30bd8a3d05
Idura1 <moneymaster316@gmail.com>
2025-02-19 20:38:11 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,19 +1,1 @@
-// dashboard.js
-import { auth } from './config.js';
-import { signOut } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
 
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    document.getElementById("userEmail").textContent = `Signed in as: ${user.email}`;
-  } else {
-    window.location.href = "login.html"; // Redirect if not authenticated
-  }
-});
-
-document.getElementById("signOutBtn").addEventListener("click", () => {
-  signOut(auth).then(() => {
-    window.location.href = "login.html"; // Redirect after sign out
-  }).catch((error) => {
-    alert("Error: " + error.message);
-  });
-});
